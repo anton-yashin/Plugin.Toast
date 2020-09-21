4ef8018636c516fb094b531b4d453ee53bef1779
Anton Yashin <ktulhu@live.com>
2020-09-21 17:06:43 +0400

    refactoring

--- src/Plugin.Toast.Images/ToastImageSourceFactory.android.cs
+++ src/Plugin.Toast.Images/ToastImageSourceFactory.android.cs
@@ -25,15 +25,12 @@ namespace Plugin.Toast
 
         async Task<ToastImageSource> CreateAsync(ImageSource imageSource, CancellationToken cancellationToken)
         {
-            var handler = Registrar.Registered.GetHandlerForObject<IImageSourceHandler>(imageSource);
-            if (handler != null)
-            {
-                var bitmap = await handler.LoadImageAsync(imageSource, Android.App.Application.Context, cancellationToken);
-                if (bitmap == null)
-                    throw new ArgumentException("Image data was invalid", nameof(imageSource));
-                return new SealedToastImageSource(bitmap);
-            }
-            throw new InvalidOperationException("please init Xamarin Forms before call this function");
+            var handler = Registrar.Registered.GetHandlerForObject<IImageSourceHandler>(imageSource)
+                ?? throw new InvalidOperationException("please init Xamarin Forms before call this function");
+            var bitmap = await handler.LoadImageAsync(imageSource, Android.App.Application.Context, cancellationToken);
+            if (bitmap == null)
+                throw new ArgumentException("Image data was invalid", nameof(imageSource));
+            return new SealedToastImageSource(bitmap);
         }
 
     }
