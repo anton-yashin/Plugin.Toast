e36ddb7b2c41d80efece7d09077329c3e89c085d
Anton Yashin <ktulhu@live.com>
2020-09-23 18:20:07 +0400

    tests for UwpImageRouter

---
+++ tests/DeviceTests.UWP/UwpImageRouter_Tests.cs
@@ -0,0 +1,49 @@
+﻿using DeviceTests.UWP.Mocks;
+using LightMock;
+using Plugin.Toast;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace DeviceTests.UWP
+{
+    public class UwpImageRouter_Tests
+    {
+        [Theory, MemberData(nameof(GetInvalidRoutes))]
+        public void ThrowsExceptionOnUnknownRoute(object route)
+        {
+            // prepare
+            var router = new UwpImageRouter();
+            var mockContext = new MockContext<IUwpExtension>();
+            var mockExtension = new MockUwpExtension(mockContext);
+            var ims = new SealedToastImageSource(new Uri("https://www.example.com/some.png"));
+
+            // act && verify
+            Assert.Throws<InvalidOperationException>(() => router.Configure(mockExtension, ims, (Router.Route)route));
+        }
+
+        public static IEnumerable<object[]> GetInvalidRoutes()
+        {
+            foreach (var i in from i in Enum.GetValues(typeof(Router.Route)).Cast<Router.Route>() where i != Router.Route.Default select i)
+                yield return new object[] { i };
+        }
+
+        [Fact]
+        public void Configure()
+        {
+            // prepare
+            var uri = new Uri("https://www.example.com/some.png");
+            var router = new UwpImageRouter();
+            var mockContext = new MockContext<IUwpExtension>();
+            var mockExtension = new MockUwpExtension(mockContext);
+            var ims = new SealedToastImageSource(uri);
+
+            // act
+            router.Configure(mockExtension, ims, Router.Route.Default);
+
+            // verify
+            mockContext.Assert(_ => _.AddAppLogoOverride(uri, null, null, null));
+        }
+    }
+}
