d8c6cf50b063a864cb2d5bd4e89fe97ee77c36c1
Anton Yashin <ktulhu@live.com>
2020-09-23 13:55:29 +0400

    move image cacher to shared code

---
+++ src/Plugin.Toast.Images/ImageCacher.netstandard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Plugin.Toast
+{
+    sealed partial class ImageCacher
+    {
+#if NETSTANDARD1_4
+        static string GetCacheFolderPath() => throw Exceptions.ExceptionUtils.NotSupportedOrImplementedException;
+#else
+        static string GetCacheFolderPath() => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+#endif
+    }
+}
