2ead091bdc608d7c159f38ca168c0962536c1e26
Anton Yashin <ktulhu@live.com>
2020-09-29 18:20:21 +0400

    add docs

--- src/Plugin.Toast.Images/IImageCacher.shared.cs
+++ src/Plugin.Toast.Images/IImageCacher.shared.cs
@@ -5,8 +5,23 @@ using System.Threading.Tasks;
 
 namespace Plugin.Toast
 {
+    /// <summary>
+    /// A caching service, used by <see cref="IToastImageSourceFactory"/> on iOS and UWP platforms
+    /// due the system restrictions
+    /// </summary>
     public interface IImageCacher
     {
+        /// <summary>
+        /// Write a data into local cache folder.
+        /// </summary>
+        /// <param name="relativePath">Path relative to temporary folder to file where a data will be stored</param>
+        /// <param name="copyToAsync">Callback which will write to provided file stream</param>
+        /// <returns>Full path to file where a cached data is stored</returns>
+        /// <remarks>
+        /// This function will create a subfolder and a file if not exists in path relative to cache folder. Then
+        /// open it and call a copyToAsync callback with a file stream as a parameter.
+        /// <seealso cref="IImageCacherExtensions"/>
+        /// </remarks>
         Task<string> CacheAsync(string relativePath, CancellationToken cancellationToken, Func<Stream, CancellationToken, Task> copyToAsync);
     }
 }
--- src/Plugin.Toast.Images/IImageCacherExtensions.shared.cs
+++ src/Plugin.Toast.Images/IImageCacherExtensions.shared.cs
@@ -5,11 +5,34 @@ using System.Threading.Tasks;
 
 namespace Plugin.Toast
 {
+    /// <summary>
+    /// Extensions to <see cref="IImageCacher"/>
+    /// </summary>
     public static class IImageCacherExtensions
     {
+        /// <summary>
+        /// Write a data to local cache folder.
+        /// </summary>
+        /// <param name="relativePath">Path relative to temporary folder to file where a data will be stored</param>
+        /// <param name="copyToAsync">Callback which will write to provided file stream</param>
+        /// <returns>Full path to file where a cached data is stored</returns>
+        /// <remarks>
+        /// This function will create a subfolder and a file if not exists in path relative to cache folder. Then
+        /// open it and call a copyToAsync callback with a file stream as a parameter.
+        /// </remarks>
         public static Task<string> CacheAsync(this IImageCacher @this, string relativePath, Func<Stream, CancellationToken, Task> copyToAsync)
             => @this.CacheAsync(relativePath, default, copyToAsync);
 
+        /// <summary>
+        /// Write a data to local cache folder.
+        /// </summary>
+        /// <param name="relativePath">Path relative to temporary folder to file where a data will be stored</param>
+        /// <param name="getStreamAsync">Provide a stream which must be written to cache</param>
+        /// <returns>Full path to file where a cached data is stored</returns>
+        /// <remarks>
+        /// This function will create a subfolder and a file if not exists in path relative to cache folder. Then
+        /// open it and call a getStreamAsync callback to copy an incomming data.
+        /// </remarks>
         public static Task<string> CacheAsync(this IImageCacher @this, string relativePath, CancellationToken cancellationToken, Func<CancellationToken, Task<Stream>> getStreamAsync)
         {
             return @this.CacheAsync(relativePath, cancellationToken, async (s, ct) =>
@@ -19,9 +42,29 @@ namespace Plugin.Toast
             });
         }
 
+        /// <summary>
+        /// Write a data to local cache folder.
+        /// </summary>
+        /// <param name="relativePath">Path relative to temporary folder to file where a data will be stored</param>
+        /// <param name="getStreamAsync">Provide a stream which must be written to cache</param>
+        /// <returns>Full path to file where a cached data is stored</returns>
+        /// <remarks>
+        /// This function will create a subfolder and a file if not exists in path relative to cache folder. Then
+        /// open it and call a getStreamAsync callback to copy an incomming data.
+        /// </remarks>
         public static Task<string> CacheAsync(this IImageCacher @this, string relativePath, Func<CancellationToken, Task<Stream>> getStreamAsync)
             => @this.CacheAsync(relativePath, default, getStreamAsync);
 
+        /// <summary>
+        /// Write a data to local cache folder.
+        /// </summary>
+        /// <param name="relativePath">Path relative to temporary folder to file where a data will be stored</param>
+        /// <param name="getStream">Provide a stream which must be written to cache</param>
+        /// <returns>Full path to file where a cached data is stored</returns>
+        /// <remarks>
+        /// This function will create a subfolder and a file if not exists in path relative to cache folder. Then
+        /// open it and call a getStream callback to copy an incomming data.
+        /// </remarks>
         public static Task<string> CacheAsync(this IImageCacher @this, string relativePath, CancellationToken cancellationToken, Func<Stream> getStream)
         {
             return @this.CacheAsync(relativePath, cancellationToken, async (s, ct) =>
@@ -31,6 +74,16 @@ namespace Plugin.Toast
             });
         }
 
+        /// <summary>
+        /// Write a data to local cache folder.
+        /// </summary>
+        /// <param name="relativePath">Path relative to temporary folder to file where a data will be stored</param>
+        /// <param name="getStream">Provide a stream which must be written to cache</param>
+        /// <returns>Full path to file where a cached data is stored</returns>
+        /// <remarks>
+        /// This function will create a subfolder and a file if not exists in path relative to cache folder. Then
+        /// open it and call a getStream callback to copy an incomming data.
+        /// </remarks>
         public static Task<string> CacheAsync(this IImageCacher @this, string relativePath, Func<Stream> getStream)
             => @this.CacheAsync(relativePath, default, getStream);
     }
