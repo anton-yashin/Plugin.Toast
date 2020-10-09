dbcbdc8e64bf8c5c9218b5a17db6d3acb8308807
Anton Yashin <ktulhu@live.com>
2020-10-09 13:33:06 +0400

    add documentation

--- src/Plugin.Toast/IDroidNotifcationChannelBuilder.shared.cs
+++ src/Plugin.Toast/IDroidNotifcationChannelBuilder.shared.cs
@@ -6,6 +6,7 @@ namespace Plugin.Toast
 {
     /// <summary>
     /// Used to setup channel for android notification using <see cref="global::Android.App.NotificationChannel"/>
+    /// <seealso cref="https://developer.android.com/reference/android/app/NotificationChannel"/>
     /// </summary>
     public interface IDroidNotifcationChannelBuilder
     {
@@ -13,23 +14,70 @@ namespace Plugin.Toast
         /// Set custom id for channel. If not set it will be generated from name.
         /// </summary>
         /// <param name="id">Channel id</param>
-        /// <returns>This object</returns>
+        /// <returns>Notification channel builder</returns>
         IDroidNotifcationChannelBuilder SetId(string id);
         /// <summary>
         /// Set name of channel. This call is mandatory. Builder will throw
         /// <see cref="InvalidOperationException"/> if call is omitted.
         /// </summary>
         /// <param name="name">Channel name</param>
-        /// <returns>This object</returns>
+        /// <returns>Notification channel builder</returns>
         IDroidNotifcationChannelBuilder SetName(string name);
+        /// <summary>
+        /// <see cref="global::Android.App.NotificationChannel.Importance"/>
+        /// <see cref="global::Android.App.NotificationChannel.NotificationChannel(string?, string?, Android.App.NotificationImportance)"/>
+        /// </summary>
+        /// <param name="notificationImportance">Notification importance</param>
+        /// <returns>Notification channel builder</returns>
         IDroidNotifcationChannelBuilder SetImportance(NotificationImportance notificationImportance);
+        /// <summary>
+        /// <see cref="global::Android.App.NotificationChannel.Group"/>
+        /// </summary>
+        /// <param name="group">Group name</param>
+        /// <returns>Notification channel builder</returns>
         IDroidNotifcationChannelBuilder SetGroup(string group);
+        /// <summary>
+        /// <see cref="global::Android.App.NotificationChannel.EnableLights(bool)"/>
+        /// </summary>
+        /// <param name="lights">lights</param>
+        /// <returns>Notification channel builder</returns>
         IDroidNotifcationChannelBuilder EnableLights(bool lights);
+        /// <summary>
+        /// <see cref="global::Android.App.NotificationChannel.EnableVibration(bool)"/>
+        /// </summary>
+        /// <param name="vibration">vibration</param>
+        /// <returns>Notification channel builder</returns>
         IDroidNotifcationChannelBuilder EnableVibration(bool vibration);
+        /// <summary>
+        /// <see cref="global::Android.App.NotificationChannel.SetAllowBubbles(bool)"/>
+        /// Android 10 is required (API 29), otherwise ignored.
+        /// </summary>
+        /// <param name="allowBubbles">Allow bubbles</param>
+        /// <returns>Notification channel builder</returns>
         IDroidNotifcationChannelBuilder SetAllowBubbles(bool allowBubbles);
+        /// <summary>
+        /// <see cref="global::Android.App.NotificationChannel.SetBypassDnd(bool)"/>
+        /// </summary>
+        /// <param name="bypassDnd">Bypass Do Not Distrub</param>
+        /// <returns>Notification channel builder</returns>
         IDroidNotifcationChannelBuilder SetBypassDnd(bool bypassDnd);
+        /// <summary>
+        /// <see cref="global::Android.App.NotificationChannel.SetShowBadge(bool)"/>
+        /// </summary>
+        /// <param name="showBadge">Show badge</param>
+        /// <returns>Notification channel builder</returns>
         IDroidNotifcationChannelBuilder SetShowBadge(bool showBadge);
+        /// <summary>
+        /// <see cref="global::Android.App.NotificationChannel.SetVibrationPattern(long[]?)"/>
+        /// </summary>
+        /// <param name="vibrationPattern">Vibration pattern</param>
+        /// <returns>Notification channel builder</returns>
         IDroidNotifcationChannelBuilder SetVibrationPattern(long[] vibrationPattern);
+        /// <summary>
+        /// <see cref="global::Android.App.NotificationChannel.Description"/>
+        /// </summary>
+        /// <param name="description">Description </param>
+        /// <returns>Notification channel builder</returns>
         IDroidNotifcationChannelBuilder SetDescription(string description);
     }
 
--- src/Plugin.Toast/ISystemEventSource.shared.cs
+++ src/Plugin.Toast/ISystemEventSource.shared.cs
@@ -1,13 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Plugin.Toast
 {
+    /// <summary>
+    /// Event source, that distributes notification events from OS to observers
+    /// </summary>
     public interface ISystemEventSource
     {
+        /// <summary>
+        /// Send events received at startup to observers.
+        /// </summary>
         void SendPendingEvents();
+        /// <summary>
+        /// Sends a event to observers.
+        /// </summary>
+        /// <param name="event">Event</param>
+        [EditorBrowsable(EditorBrowsableState.Never)]
         void SendEvent(NotificationEvent @event);
+        /// <summary>
+        /// Stores a weak reference to observer, to deliver events later.
+        /// </summary>
+        /// <param name="observer">Observer</param>
         void Subscribe(INotificationEventObserver observer);
     }
 }
