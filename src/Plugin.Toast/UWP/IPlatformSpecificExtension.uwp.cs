32d9916f2d8227dda46bc15d1170620be7aa505f
Anton Yashin <ktulhu@live.com>
2021-04-19 17:37:30 +0400

    docs: documentation added

--- src/Plugin.Toast/UWP/IPlatformSpecificExtension.uwp.cs
+++ src/Plugin.Toast/UWP/IPlatformSpecificExtension.uwp.cs
@@ -1,10 +1,11 @@
-﻿using Plugin.Toast.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
 
 namespace Plugin.Toast.UWP
 {
+    /// <summary>
+    /// The interface is proxy for <see cref="Microsoft.Toolkit.Uwp.Notifications.ToastContentBuilder"/>
+    /// and contains functions specific to UWP.
+    /// </summary>
     public interface IPlatformSpecificExtension : IUwpExtension, INotificationBuilderExtension<IPlatformSpecificExtension>
     {
     }
