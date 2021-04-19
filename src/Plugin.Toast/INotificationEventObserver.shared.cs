97f7c0da825c6479cb896e78c5c6323af8f2aa5c
Anton Yashin <ktulhu@live.com>
2021-04-19 15:12:05 +0400

    docs: documentation added

--- src/Plugin.Toast/INotificationEventObserver.shared.cs
+++ src/Plugin.Toast/INotificationEventObserver.shared.cs
@@ -1,11 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Plugin.Toast
 {
+    /// <summary>
+    /// Notification event observer. You can subscribe to receive events using <see cref="ISystemEventSource"/>
+    /// </summary>
     public interface INotificationEventObserver
     {
+        /// <summary>
+        /// The method that is called when <see cref="NotificationEvent"/> is occurs.
+        /// </summary>
         void OnNotificationReceived(NotificationEvent @event);
     }
 }
--- src/Plugin.Toast/NotificationEvent.shared.cs
+++ src/Plugin.Toast/NotificationEvent.shared.cs
@@ -1,13 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Plugin.Toast
 {
+    /// <summary>
+    /// Notification event.
+    /// </summary>
     public sealed class NotificationEvent : EventArgs
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotificationEvent"/> class.
+        /// </summary>
+        /// <param name="toastId"></param>
         public NotificationEvent(ToastId toastId) => ToastId = toastId;
 
+        /// <summary>
+        /// Identifier of notification linked with this event.
+        /// </summary>
         public ToastId ToastId { get; }
     }
 }
