b5377f71890d99f2891e1e24d2f0868ed291e1a7
Anton Yashin <ktulhu@live.com>
2021-04-20 15:37:27 +0400

    fix: typo

--- src/Plugin.Toast.Images/BundleToFileNameStrategy.shared.cs
+++ src/Plugin.Toast.Images/BundleToFileNameStrategy.shared.cs
@@ -5,7 +5,7 @@ namespace Plugin.Toast
 {
     sealed class BundleToFileNameStrategy : IBundleToFileNameStrategy
     {
-        internal const string KFolder = "ToastImageSouce.FromBundle/";
+        internal const string KFolder = "ToastImageSource.FromBundle/";
 
         public string Convert(string bundlePath)
             => Path.Combine(KFolder, bundlePath);
@@ -14,10 +14,17 @@ namespace Plugin.Toast
     /// <summary>
     /// Provides conversion from bundle file name to local file name.
     /// You can override default behaviour by registering in 
-    /// IoC a custom implementation.
+    /// IoC a custom implementation. Used by iOS implementation of
+    /// <see cref="IToastImageSourceFactory.FromFileAsync(string, System.Threading.CancellationToken)"/>
+    /// to cache bundle images.
     /// </summary>
     public interface IBundleToFileNameStrategy
     {
+        /// <summary>
+        /// Provides conversion from bundle file name to local file name.
+        /// </summary>
+        /// <param name="bundlePath"></param>
+        /// <returns></returns>
         string Convert(string bundlePath);
     }
 }
