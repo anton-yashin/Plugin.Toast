c7af4dbd837173e52631a3c48234a7e8869bbe15
Anton Yashin <ktulhu@live.com>
2021-06-15 17:35:48 +0400

    tests: allow to run tests in parallel.

--- tests/DeviceTests.Android/Properties/AssemblyInfo.cs
+++ tests/DeviceTests.Android/Properties/AssemblyInfo.cs
@@ -25,4 +25,3 @@ using Xunit;
 //      Revision
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: CollectionBehavior(DisableTestParallelization = true)]
