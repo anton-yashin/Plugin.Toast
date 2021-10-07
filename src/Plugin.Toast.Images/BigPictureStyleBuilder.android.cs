46cee381d611ed9155ed48a0de4f273ccfb96c8b
Anton Iashin <ktulhu@live.com>
2021-10-07 18:35:27 +0400

    feat: BigPictureStyle builder

---
+++ src/Plugin.Toast.Images/BigPictureStyleBuilder.android.cs
@@ -0,0 +1,39 @@
+﻿using AndroidX.Core.App;
+using System;
+
+namespace Plugin.Toast.Images
+{
+    sealed class BigPictureStyleBuilder : IBigPictureStyle
+    {
+        private readonly NotificationCompat.BigPictureStyle style;
+
+        public BigPictureStyleBuilder() => style = new NotificationCompat.BigPictureStyle();
+
+        public IBigPictureStyle BigLargeIcon(ToastImageSource b)
+        {
+            style.BigLargeIcon(b.Bitmap);
+            return this;
+        }
+
+        public IBigPictureStyle BigPicture(ToastImageSource b)
+        {
+            style.BigPicture(b.Bitmap);
+            return this;
+        }
+
+        public IBigPictureStyle SetBigContentTitle(string title)
+        {
+            style.SetBigContentTitle(title);
+            return this;
+        }
+
+        public IBigPictureStyle SetSummaryText(string cs)
+        {
+            style.SetSummaryText(cs);
+            return this;
+        }
+
+        public NotificationCompat.Style Build() => style;
+
+    }
+}
