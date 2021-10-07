254b54cb6ccb38f4a6db09940f4ed9924f20cb65
Anton Iashin <ktulhu@live.com>
2021-10-07 18:16:44 +0400

    feeat: add interfaces for builders with images

---
+++ src/Plugin.Toast.Images/IDroidPersonBuilder.shared.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.Toast.Images
+{
+    /// <summary>
+    /// Abstracton layer for android person builder.
+    /// </summary>
+    public interface IDroidPersonBuilder : IDroidStyleBuilder
+    {
+        /// <summary>
+        /// Sets whether or not this Person represents a machine rather than a human. 
+        /// </summary>
+        /// <remarks>
+        /// Portions of this page are reproduced from work created and shared by
+        /// the Android Open Source Project and used according to terms described
+        /// in the Creative Commons 2.5 Attribution License. 
+        /// </remarks>
+        IDroidPersonBuilder SetBot(bool bot);
+        /// <summary>
+        /// Set an icon for this Person.
+        /// </summary>
+        /// <remarks>
+        /// Portions of this page are reproduced from work created and shared by
+        /// the Android Open Source Project and used according to terms described
+        /// in the Creative Commons 2.5 Attribution License. 
+        /// </remarks>
+        IDroidPersonBuilder SetIcon(ToastImageSource icon);
+        /// <summary>
+        /// Sets whether this is an important person. 
+        /// </summary>
+        /// <remarks>
+        /// Portions of this page are reproduced from work created and shared by
+        /// the Android Open Source Project and used according to terms described
+        /// in the Creative Commons 2.5 Attribution License. 
+        /// </remarks>
+        IDroidPersonBuilder SetImportant(bool important);
+        /// <summary>
+        /// Set a unique identifier for this Person.
+        /// </summary>
+        /// <remarks>
+        /// Portions of this page are reproduced from work created and shared by
+        /// the Android Open Source Project and used according to terms described
+        /// in the Creative Commons 2.5 Attribution License. 
+        /// </remarks>
+        IDroidPersonBuilder SetKey(string key);
+        /// <summary>
+        /// Give this Person a name to use for display. 
+        /// </summary>
+        /// <remarks>
+        /// Portions of this page are reproduced from work created and shared by
+        /// the Android Open Source Project and used according to terms described
+        /// in the Creative Commons 2.5 Attribution License. 
+        /// </remarks>
+        IDroidPersonBuilder SetName(string name);
+        /// <summary>
+        /// Set a URI for this Person which can be any of the following:
+        /// <list type="bullet">
+        /// <item>
+        /// The String representation of a
+        /// <see href="https://developer.android.com/reference/android/provider/ContactsContract.Contacts.html#CONTENT_LOOKUP_URI">ContactsContract.Contacts.CONTENT_LOOKUP_URI</see>
+        /// </item>
+        /// <item>
+        /// A mailto: schema*
+        /// </item>
+        /// <item>
+        /// A tel: schema*
+        /// </item>
+        /// </list>
+        /// * Note for these schemas, the path portion of the URI must exist in the
+        /// contacts database in their appropriate column, otherwise the reference will be discarded. 
+        /// </summary>
+        /// <remarks>
+        /// Portions of this page are reproduced from work created and shared by
+        /// the Android Open Source Project and used according to terms described
+        /// in the Creative Commons 2.5 Attribution License. 
+        /// </remarks>
+        IDroidPersonBuilder SetUri(string uri);
+    }
+}
---
+++ src/Plugin.Toast.Images/IMessagingStyle.shared.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.Toast.Images
+{
+    /// <summary>
+    /// Style for generating large-format notifications that
+    /// include multiple back-and-forth messages of varying types
+    /// between any number of people.
+    /// In order to get a backwards compatible behavior, the app
+    /// needs to use the v7 version of the notification builder
+    /// together with this style, otherwise the user will see the
+    /// normal notification view.
+    /// Use <see cref="SetConversationTitle(string)"/> to set a
+    /// conversation title for group chats with more than two people.
+    /// This could be the user-created name of the group or, if it
+    /// doesn't have a specific name, a list of the participants in
+    /// the conversation. Do not set a conversation title for one-on-one
+    /// chats, since platforms use the existence of this field as a
+    /// hint that the conversation is a group. 
+    /// </summary>
+    /// <remarks>
+    /// Portions of this page are reproduced from work created and shared by
+    /// the Android Open Source Project and used according to terms described
+    /// in the Creative Commons 2.5 Attribution License. 
+    /// </remarks>
+    public interface IMessagingStyle : IDroidStyleBuilder
+    {
+        /// <summary>
+        /// Creates a new <see cref="AndroidX.Core.App.NotificationCompat.MessagingStyle"/>  object. 
+        /// </summary>
+        IMessagingStyleStep2 With(Action<IDroidPersonBuilder> personBuilder);
+    }
+
+    /// <summary>
+    /// Optional fields for <see cref="IMessagingStyle"/>
+    /// </summary>
+    public interface IMessagingStyleStep2
+    {
+        /// <summary>
+        /// Adds a message for display by this notification.
+        /// </summary>
+        /// <remarks>
+        /// Portions of this page are reproduced from work created and shared by
+        /// the Android Open Source Project and used according to terms described
+        /// in the Creative Commons 2.5 Attribution License. 
+        /// </remarks>
+        IMessagingStyle AddMessage(string text, long timestamp, Action<IDroidPersonBuilder> personBuilder);
+        /// <summary>
+        /// Sets the title to be displayed on this conversation. 
+        /// </summary>
+        /// <remarks>
+        /// Portions of this page are reproduced from work created and shared by
+        /// the Android Open Source Project and used according to terms described
+        /// in the Creative Commons 2.5 Attribution License. 
+        /// </remarks>
+        IMessagingStyle SetConversationTitle(string conversationTitle);
+        /// <summary>
+        /// Sets whether this conversation notification represents a group. 
+        /// </summary>
+        /// <remarks>
+        /// Portions of this page are reproduced from work created and shared by
+        /// the Android Open Source Project and used according to terms described
+        /// in the Creative Commons 2.5 Attribution License. 
+        /// </remarks>
+        IMessagingStyle SetGroupConversation(bool isGroupConversation);
+    }
+
+}
