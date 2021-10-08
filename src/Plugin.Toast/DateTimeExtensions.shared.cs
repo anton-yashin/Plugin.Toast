25fe2b0f6f57f96eda765e74aedc645290ecb1a5
Anton Iashin <ktulhu@live.com>
2021-10-08 16:47:32 +0400

    refactor: DateTime to android timestamp converter

---
+++ src/Plugin.Toast/DateTimeExtensions.shared.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Plugin.Toast
+{
+    internal static class DateTimeExtensions
+    {
+        public static long ToAndroidTimeStamp(this DateTime @this)
+        {
+            const long KUnixEpochBase = 62135596800000L;
+            return @this.Ticks / 10000 - KUnixEpochBase;
+        }
+    }
+}
