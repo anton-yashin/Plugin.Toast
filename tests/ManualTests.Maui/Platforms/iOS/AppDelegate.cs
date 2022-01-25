c56a8fb18ec6fa93380dc5b8d6d8aa7925f95d4d
Anton Iashin <ktulhu@live.com>
2022-01-25 20:44:41 +0400

    tests: handle activation by notification on maui

--- tests/ManualTests.Maui/Platforms/iOS/AppDelegate.cs
+++ tests/ManualTests.Maui/Platforms/iOS/AppDelegate.cs
@@ -1,6 +1,8 @@
 ﻿using Foundation;
 using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
+using Plugin.Toast;
+using UIKit;
 
 namespace ManualTests.Maui
 {
@@ -8,5 +10,11 @@ namespace ManualTests.Maui
     public class AppDelegate : MauiUIApplicationDelegate
     {
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            Platform.OnActivated(application, launchOptions);
+            return base.FinishedLaunching(application, launchOptions);
+        }
     }
 }
